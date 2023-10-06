aae9845a66cec052e355f9507dc9556e41d965b6
elenalev2686 <elenalev2686@gmail.com>
2023-10-06 23:31:08 +0700

    changed src

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -2,6 +2,7 @@ package ru.netology.service;
 
 import org.testng.annotations.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.testng.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
